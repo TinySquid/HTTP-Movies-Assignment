b3e3f91873362deb2b3d4d6f159502dd9bf827be
Michael Nunes <miketnunes@gmail.com>
2019-11-14 10:20:01 -0800

    Add form control handlers

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
-/*
-Format to send to server endpoint
-  id: 4,
-  title: "Dumb and Dumber",
-  director: "The Farely Brothers",
-  metascore: 76,
-  stars: ["Jim Carrey", "Jeff Daniels", "Lauren Holly"]
-*/
 
 const MovieUpdate = props => {
+  /*
+  Format to send to server endpoint
+    id: 4,
+    title: "Dumb and Dumber",
+    director: "The Farely Brothers",
+    metascore: 76,
+    stars: ["Jim Carrey", "Jeff Daniels", "Lauren Holly"]
+*/
   const [inputs, setInputs] = useState({
     id: null,
     title: '',
@@ -26,26 +26,60 @@ const MovieUpdate = props => {
       .catch(err => console.log(err.response));
   }, []);
 
+  const handleChange = e => {
+    //Shorter name
+    const input = e.target;
+
+    if (input.name !== "stars") {
+      setInputs({
+        ...inputs,
+        [input.name]: input.value
+      });
+    } else {
+      //Get correct id of actor input from data attribute
+      const id = input.getAttribute('data-id');
+
+      //Make a new array and spread existing stars in it
+      const newStars = [...inputs.stars];
+
+      //Alter just the star input we are editing
+      newStars[id] = input.value;
+
+      //Update state
+      setInputs({
+        ...inputs,
+        stars: newStars
+      });
+    }
+
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.updateMovie(inputs);
+  }
+
   return (
-    <form className="update-movie">
+    <form className="update-movie" onSubmit={handleSubmit}>
       <h1>Update Movie Details</h1>
       <label> Movie Title
-      <input type="text" name="title" value={inputs.title} placeholder="Movie title" />
+      <input type="text" name="title" value={inputs.title} onChange={handleChange} />
       </label>
       <label>
         Director
-      <input type="text" name="director" value={inputs.director} />
+      <input type="text" name="director" value={inputs.director} onChange={handleChange} />
       </label>
       <label>
         Metascore
-      <input type="text" name="metascore" value={inputs.metascore} />
+      <input type="text" name="metascore" value={inputs.metascore} onChange={handleChange} />
       </label>
       <h2>Actors</h2>
       {inputs.stars.map((star, idx) => (
-        <input key={idx} type="text" name="stars" value={inputs.stars[idx]} placeholder="Actor name" />
+        <input key={idx} data-id={idx} type="text" name="stars" value={inputs.stars[idx]} onChange={handleChange} />
       ))}
+      <button type="submit">Submit</button>
     </form>
   )
 }
 
-export default MovieUpdate
+export default MovieUpdate;
